fc2f757e76811919432f0b81004256392c6928c2
David Buchmann <david.buchmann@liip.ch>
2019-06-05 10:28:42 +0200

    drop hhvm and test with recent php versions

--- spec/ContentLengthPluginSpec.php
+++ spec/ContentLengthPluginSpec.php
@@ -28,10 +28,6 @@ class ContentLengthPluginSpec extends ObjectBehavior
 
     function it_streams_chunked_if_no_size(RequestInterface $request, StreamInterface $stream)
     {
-        if(defined('HHVM_VERSION')) {
-            throw new SkippingException('Skipping test on hhvm, as there is no chunk encoding on hhvm');
-        }
-
         $request->hasHeader('Content-Length')->shouldBeCalled()->willReturn(false);
         $request->getBody()->shouldBeCalled()->willReturn($stream);
 
